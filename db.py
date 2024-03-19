3c21e10b224641754357e5cd0f76de089316952d
sabinKrafton <160460876+sabinKrafton@users.noreply.github.com>
2024-03-19 16:31:09 +0900

    index, db upload
    초안

---
+++ db.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)  
+db = client.whatKindBird
+
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
+db.bird.insert_one({'name' : '도요새', 'image' : 'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160712_285%2Fwhite_hongssi_1468253087152Excxs_JPEG%2F01A.jpg&type=sc960_832'})
