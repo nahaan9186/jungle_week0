424d084adbadf5e3ff7645449325f15a36fa7cbc
Shy9419 <160864944+Shy9419@users.noreply.github.com>
2024-03-20 22:33:03 +0900

    거의 최종

--- db.py
+++ db.py
@@ -1,11 +1,15 @@
 from bson import ObjectId
 from pymongo import MongoClient
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, make_response
 from flask.json.provider import JSONProvider
 
 import json
 import sys
+
+import jwt
+import datetime
+
 import random
 
 
@@ -14,9 +18,8 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.lookFace
 
-#####################################################################################
-# 이 부분은 코드를 건드리지 말고 그냥 두세요. 코드를 이해하지 못해도 상관없는 부분입니다.
-#
+all_user = list(db.users.find({}))
+
 # ObjectId 타입으로 되어있는 _id 필드는 Flask 의 jsonify 호출시 문제가 된다.
 # 이를 처리하기 위해서 기본 JsonEncoder 가 아닌 custom encoder 를 사용한다.
 # Custom encoder 는 다른 부분은 모두 기본 encoder 에 동작을 위임하고 ObjectId 타입만 직접 처리한다.
@@ -38,60 +41,90 @@ class CustomJSONProvider(JSONProvider):
 # 위에 정의되 custom encoder 를 사용하게끔 설정한다.
 app.json = CustomJSONProvider(app)
 
-# 여기까지 이해 못해도 그냥 넘어갈 코드입니다.
-# #####################################################################################
+app.config['SECRET_KEY'] = 'user_secret_key'
+
+app.route('/check_cookie')
+def check_cookie():
+    jwt_token = request.cookies.get('jwt_token')
+    if jwt_token:
+        return f"JWT 토큰이 쿠키게 저장되어 있습니다: (jwt_token)"
+    else:
+        return "쿠키에 JWT 토큰이 저장되어 있지 않습니다."
+
+@app.route('/index.html')
+def base():
+    return render_template('index.html')
 
 @app.route('/')
-def home():
-    return render_template('login.html')
-
-@app.route('/sign.html')
-def sign_page():
-    return render_template('sign.html')
-
-@app.route('/gameStart.html')
-def gamestrat_page():
-    return render_template('gameStart.html')
+def login_page():
+    title = '로그인 페이지'
+    return render_template('login.html', title=title)
 
 @app.route('/game.html')
 def game_page():
-    return render_template('game.html')
+    title = '게임 페이지'
+    return render_template('game.html', title=title)
 
+@app.route('/gameStart.html')
+def gameStart_page():
+    title = '게임 페이지'
+    return render_template('gameStart.html', title=title)
+
+@app.route('/sign.html')
+def sign_page():
+    title = '회원가입 페이지'
+    return render_template('sign.html', title=title)
 
 @app.route('/api/sign', methods = ['GET','POST'])
 def sign_up():
     name_receive = request.form.get('give_name')
     img_receive = request.form.get('give_img')
     id_receive = request.form.get('give_userId')
-    registered_id = db.users.find_one({'id' : id_receive })
-    if registered_id:
-        return jsonify({'result': 'failure'})
     password_receive = request.form.get('give_password')
+    
+    # 미기입 항목 확인
+    if (name_receive == '') or (img_receive == '') or (id_receive == '') or (password_receive == ''):
+        return jsonify({'result': 'itemMissing'})
+    
+    # 중복된 ID 확인
+    registered_id = db.users.find_one({'_id' : id_receive})
+    if registered_id:
+        return jsonify({'result': 'duplication'})
 
-    doc = { 'name' : name_receive, 'img' : img_receive, 'id' : id_receive, 'password' : password_receive}
+    doc = { '_id' : id_receive, 'name' : name_receive, 'img' : img_receive, 'score' : 0, 'password' : password_receive}
 
     db.users.insert_one(doc)
+    
+    
+    # 회원가입이 성공하면 JWT 토큰 생성
+    token = jwt.encode({'password': password_receive, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm='HS256')
+    
+    # 토큰을 쿠키에 저장
+    response = make_response(jsonify({'result': 'success'}))
+    response.set_cookie('jwt_token', token)
 
-    return jsonify({'result' : 'success'})
+    return response
+
+    # return jsonify({'result': 'success'})
+    
 
 @app.route('/api/login', methods = ['GET','POST'])
 def login():
     login_id = request.form.get('give_id')
     login_password = request.form.get('give_password')
-    registered_user = db.users.find_one({'id' : login_id })
+    registered_user = db.users.find_one({'_id' : login_id })
     if not registered_user:
-
         return jsonify({'result': 'failure'})
     if registered_user['password'] == login_password:
         return jsonify({'result' : 'success'})
     else:
         return jsonify({'result': 'failure'})
-    
+ 
 @app.route('/api/list', methods = ['GET'])
 def addProblem():
     mateList = list(db.users.find({}))
     return jsonify({'result' : 'success', 'mateList' : mateList})
-
+ 
 if __name__ == '__main__':
     print(sys.executable)
     app.run('0.0.0.0', port=5000, debug=True)
