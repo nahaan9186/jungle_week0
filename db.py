a47003a908ad1ef6679f99c7d074ab7457777bd1
sabinKrafton <160460876+sabinKrafton@users.noreply.github.com>
2024-03-21 12:03:35 +0900

    민사빈
    로그인 하면 세션에 정보 등록, 로그아웃 하면 세션에서 정보 지우기,  랭킹페이지에 정보 올리기 진행 중

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 from pymongo import MongoClient
 
-from flask import Flask, render_template, jsonify, request, make_response
+from flask import Flask, render_template, jsonify, request, make_response, session
 from flask.json.provider import JSONProvider
 
 import json
@@ -10,13 +10,16 @@ import sys
 import jwt
 import datetime
 
+import random
+
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.lookFace
 
-all_user = list(db.users.find({}))
+# 세션에 사용될 시크릿 키 설정
+app.secret_key = 'user_secret_key'
 
 # ObjectId 타입으로 되어있는 _id 필드는 Flask 의 jsonify 호출시 문제가 된다.
 # 이를 처리하기 위해서 기본 JsonEncoder 가 아닌 custom encoder 를 사용한다.
@@ -68,36 +71,17 @@ def gameStart_page():
     title = '게임 페이지'
     return render_template('gameStart.html', title=title)
 
-# 랭킹 페이지 화면 랜더링
-@app.route('/ranking.html')
-def ranking_page():
-    title = '랭킹 페이지'
-    return render_template('ranking.html', title=title)
-
-# 랭킹 API 역할을 하는 부분
-@app.route('/api/ranking', methods = ['POST'])
-def ranking():
-    score_receive = request.form.get('give_score')
-    
-    db.users.update_one({'score': score_receive})
-    
-    return jsonify({'result': 'success'})
-
-# 랭킹 API 역할을 하는 부분
-@app.route('/api/ranking', methods = ['GET'])
-def show_score():
-    score = list(db.users.find({}, {'_id':False}, {'password':False}, {'id':False}.sort('score',-1)))
-    return jsonify({'result': 'success', 'score_list':score})
-    
-
-# 회원가입 화면 보여주기
 @app.route('/sign.html')
 def sign_page():
     title = '회원가입 페이지'
     return render_template('sign.html', title=title)
 
-# 회원가입 API 역할을 하는 부분
-@app.route('/api/sign', methods = ['POST'])
+@app.route('/ranking.html')
+def rank_page():
+    title = '랭킹 페이지'
+    return render_template('ranking.html', title = title)
+
+@app.route('/api/sign', methods = ['GET','POST'])
 def sign_up():
     name_receive = request.form.get('give_name')
     img_receive = request.form.get('give_img')
@@ -130,7 +114,7 @@ def sign_up():
     # return jsonify({'result': 'success'})
     
 
-@app.route('/login.html', methods = ['GET','POST'])
+@app.route('/api/login', methods = ['GET','POST'])
 def login():
     login_id = request.form.get('give_id')
     login_password = request.form.get('give_password')
@@ -138,14 +122,49 @@ def login():
     if not registered_user:
         return jsonify({'result': 'failure'})
     if registered_user['password'] == login_password:
+        # 로그인에 성공하면 세션에 사용자 정보 저장
+        session['user'] = registered_user
+        # print('session : ', session)
         return jsonify({'result' : 'success'})
     else:
         return jsonify({'result': 'failure'})
+    
+@app.route('/logout')
+def logout():
+    # 세션에서 사용자 정보 삭제
+    session.pop('user', None)
+    print('로그아웃!')
+    return jsonify({'result': 'success'})
  
 @app.route('/api/list', methods = ['GET'])
 def addProblem():
     mateList = list(db.users.find({}))
     return jsonify({'result' : 'success', 'mateList' : mateList})
+
+@app.route('/api/rank', methods = ['POST'])
+def addScore():
+    receive_score = request.form.get('give_score')
+    # 세션에서 현재 로그인한 사용자 정보 가져오기
+    current_user = session.get('user')
+
+    # 사용자 정보가 세션에 있는지 확인하고, 있으면 해당 사용자의 score를 업데이트
+    if current_user:
+        db.users.update_one({'_id': current_user['_id']}, {'$set': {'score': receive_score}})
+        return jsonify({'result': 'success'})
+    else:
+        return jsonify({'result': 'failure', 'message': '유저 정보를 찾을 수 없습니다.'})
+
+@app.route('/api/ranking')
+def show_score():
+    score = list(db.users.find({}))
+    return jsonify({'result' : 'success', 'score_list' : score})
+
+# @app.route('/ranking.html')
+# def rank_page():
+#     # 유저 정보를 가져와서 점수순으로 정렬
+#     users = db.users.find().sort([('score', -1)])
+#     # 랭킹 페이지 렌더링
+#     return render_template('ranking.html', users=users)
  
 if __name__ == '__main__':
     print(sys.executable)
